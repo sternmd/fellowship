0010dee4ff6eaaf2025e96ecf2894be72c9d0984
Max Stern <stern.maxwell@gmail.com>
2016-03-04 17:58:21 -0600

    Weekend HW parts 1-6.

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -20,4 +20,76 @@ var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
 
+//PART 1
+var makeMiddleEarth = function() {
 
+// create section tag with an id of middle earth...
+ var middleEarth = $('<section id="middle-earth"></section>');
+
+// add each land as article tag & inside each land make an H1
+  for (var i = 0; i < lands.length; i++) {
+    $(middleEarth).append('<article><h1>' + lands[i] + '</h1></article>');
+  }
+
+// append middle earth to the body
+  $('body').append(middleEarth)
+};
+
+makeMiddleEarth()
+
+//PART 2
+
+var makeHobbits = function() {
+   // display an `unordered list` of hobbits in the shire
+   var unorderedList = $('<ul id="hobbit"></ul>');
+
+   for (var i = 0; i < hobbits.length; i++)  {
+     $(unorderedList).append('<li>' + hobbits[i] + '</li>');
+   }
+  // (which is the second article tag on the page)
+  $("article:nth-child(1)").append(unorderedList);
+  // give each hobbit a class of `hobbit`
+};
+
+makeHobbits()
+
+//PART 3
+
+var keepItSecretKeepItSafe = function() {
+   // create a div with an id of `'the-ring'
+   // give the div a class of `'magic-imbued-jewelry'
+   var ring = $("<div id='the-ring' class='magic-imbued-jewelry'></div>");
+   // add the ring as a child of Frodo
+   $("li:nth-child(1)").append(ring);
+};
+
+keepItSecretKeepItSafe()
+
+
+//PART 4
+var makeBuddies = function () {
+    // create an `aside` tag
+    var aside = $('<aside></aside>');
+    // attach an `unordered list` of the `'buddies'` in the aside
+    for (var i = 0; i < buddies.length; i++)  {
+      $(aside).append('<ul>' + buddies[i] + '</ul>');
+    }
+   // insert your aside as a child element of `rivendell`
+   $("article:nth-child(2)").append(aside);
+};
+
+makeBuddies();
+
+//PART 5
+var beautifulStranger = function () {
+ $('aside ul').eq(3).html('Aragon');
+  // change the `'Strider'` text to `'Aragorn'`
+};
+
+beautifulStranger();
+
+//Part 6
+=
+var leaveTheShire = function () {
+   // assemble the `hobbits` and move them to `rivendell`
+};
